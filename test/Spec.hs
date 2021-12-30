6635ecfcd118310ae7bf93e89e0e7ff3500e6a2f
ostnam <tsougaevm@gmail.com>
2021-12-30 17:49:23 +0100

    started writing tests!

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,23 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
+import qualified Parsing
+import qualified Values
+
+arbitraryString :: String -> Bool 
+arbitraryString a = Values.showVal (Values.String a) == ("\"" ++ a ++ "\"")
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $
+  describe "Values.showVal" $ do
+  it "prints atoms" $ do
+    Values.showVal (Values.Atom "var_name") `shouldBe` "var_name"
+  it "prints strings" $ do
+    Values.showVal (Values.String "Hello!") `shouldBe` "\"Hello!\""
+  it "prints arbitrary strings" $ do
+    property  arbitraryString
+  it "prints booleans" $ do
+    Values.showVal (Values.Bool True) `shouldBe` "True"
+    Values.showVal (Values.Bool False) `shouldBe` "False"
+
