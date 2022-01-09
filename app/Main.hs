2c748228a8162d6613f8fa50388ae2637ee3dc36
ostnam <tsougaevm@gmail.com>
2022-01-09 13:38:23 +0100

    added basic REPL

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,16 @@ module Main where
 import qualified System.Environment
 import Parsing
 import Values
+import Repl
 
 main :: IO ()
 main = do
   args <- System.Environment.getArgs
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ head args
+    _ -> putStrLn "Program takes 0 or 1 argument"
+  {-
   let evaled = fmap show $ eval $ readExpr (head args)
   print evaled
+  -}
